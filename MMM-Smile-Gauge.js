37d99bf06bb84b1f91dbe737022d704a255f7c22
Wen-Hao Lee <wing3s@gmail.com>
2019-08-29 02:45:39 -0700

    increase gauge sensitivity

--- MMM-Smile-Gauge.js
+++ MMM-Smile-Gauge.js
@@ -5,7 +5,7 @@ var currTs = function () {
 Module.register("MMM-Smile-Gauge", {
     defaults: {
         sliderWidth: "450",  //px
-        gaugeSensitivity: 0.8, // 0.1~1.0 Higher sensitivity moves gauge faster
+        gaugeSensitivity: 1.1, // 0.1~2.0 Higher sensitivity moves gauge faster
         resetThreshold: 4, // Number of sec not seeing faces to reset gauge
         pauseThreshold: 3, // Number of sec not receiving detector signals to pause preview, set 0 to keep preview always on
         gaugeLeftText: "ChuChu's poker face", // show text when gauge reaches 0
